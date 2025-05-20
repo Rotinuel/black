e04439f4935e877e74158c6af2ff1c085f4511ea
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2025-05-20 12:31:26 +0100

    quickpush

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,10 @@
+"use client"
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="absolute top-0 left-0 w-full bg-transparent p-6 text-black">
       <div className="container mx-auto flex justify-between items-center">
@@ -18,7 +21,34 @@ const Navbar = () => {
         <Link href="/get-started">
           <button className="bg-[#2e2e2e] text-white px-4 py-2 rounded hover:bg-[#434343]">Get Started</button>
         </Link>
+        {/* Hamburger Menu Button */}
+        <button className="md:hidden focus:outline-none" onClick={() => setIsOpen(!isOpen)}>
+          <div className="space-y-1">
+            <span className="block w-6 h-1 bg-white"></span>
+            <span className="block w-6 h-1 bg-white"></span>
+            <span className="block w-6 h-1 bg-white"></span>
+          </div>
+        </button>
       </div>
+      {/* Mobile Menu */}
+      {isOpen && (
+        <div className="md:hidden absolute top-16 left-0 w-full bg-purple-600 text-white">
+          <ul className="flex flex-col items-center space-y-4 p-6">
+            {["Home", "About Us", "Services", "Pages", "Blog", "Contact"].map((item, index) => (
+              <li key={index}>
+                <Link href={`/${item.toLowerCase()}`} className="hover:text-gray-300">
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <Link href="/get-started">
+            <button className="bg-white text-purple-600 px-4 py-2 rounded w-full mt-4">
+              Get Started
+            </button>
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
